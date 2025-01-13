2cb2c549f48e7bdaef95dcbbc0e8d559c777f5cd
3v0k4 <riccardo.odone@gmail.com>
2025-01-13 10:05:33 +0100

    nav active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def active_link_to(name = nil, options = nil, html_options = nil, &block)
+    if options.is_a?(String) && request.path == options
+      html_options = html_options.to_h.merge(class: "active") do |_, old, new|
+        [ *Array(old), new ].join(" ")
+      end
+      link_to(name, options, html_options, &block)
+    else
+      link_to(name, options, html_options, &block)
+    end
+  end
 end
