1c70717905a669a34b900858534e8a376cfa6a51
3v0k4 <riccardo.odone@gmail.com>
2025-01-20 10:18:28 +0100

    nav

--- app/javascript/controllers/accordion_controller.js
+++ app/javascript/controllers/accordion_controller.js
@@ -2,18 +2,35 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static values = {
-    klass: String,
-    open: Boolean,
+    expanded: Boolean,
+    expandedClass: String,
+    collapsedAriaLabel: String,
+    expandedAriaLabel: String,
   };
 
+  static targets = ["toggle"];
+
   connect() {
-    if (!this.openValue) {
-      return;
+    this._setStatus();
+
+    if (this.expandedValue) {
+      this.element.classList.add(this.expandedClassValue);
     }
-    this.element.classList.add(this.klassValue);
   }
 
-  toggle(event) {
-    this.element.classList.toggle(this.klassValue);
+  toggle() {
+    this.expandedValue = !this.expandedValue;
+    this._setStatus();
+    this.element.classList.toggle(this.expandedClassValue);
+  }
+
+  _setStatus() {
+    if (this.expandedValue) {
+      this.toggleTarget.ariaExpanded = true;
+      this.toggleTarget.ariaLabel = this.expandedAriaLabelValue;
+    } else {
+      this.toggleTarget.ariaExpanded = false;
+      this.toggleTarget.ariaLabel = this.collapsedAriaLabelValue;
+    }
   }
 }
