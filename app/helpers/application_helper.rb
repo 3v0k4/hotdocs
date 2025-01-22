b62087f007f9a650e62766c0f6226e2ed7a9caaa
3v0k4 <riccardo.odone@gmail.com>
2025-01-22 10:05:59 +0100

    external link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,22 @@ module ApplicationHelper
     end
   end
 
+  def external_link_to(name = nil, options = nil, html_options = nil)
+    html_options = html_options.to_h.merge(class: "external-link") do |_, old, new|
+      [ *Array(old), new ].join(" ")
+    end
+
+    link_to(options, html_options) do
+      concat(content_tag(:span, name))
+
+      concat(<<~SVG.html_safe)
+        <svg aria-hidden="true" viewBox="0 0 24 24" class="external-link__icon">
+          <path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path>
+        </svg>
+      SVG
+    end
+  end
+
   def active_link?(url)
     request.path == url
   end
