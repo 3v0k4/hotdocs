8f52fa173420dcf6b4922181f62e4580c3664a54
3v0k4 <riccardo.odone@gmail.com>
2025-02-03 10:13:58 +0100

    wip

--- app/controllers/hotdocs_controller.rb
+++ app/controllers/hotdocs_controller.rb
@@ -1,30 +1,3 @@
 class HotDocsController < ApplicationController
   layout "hotdocs"
-
-  def index
-  end
-
-  def quickstart
-  end
-
-  def embedded
-  end
-
-  def standalone
-  end
-
-  def toc
-  end
-
-  def markdown
-  end
-
-  def static_export
-  end
-
-  def search
-  end
-
-  def light_dark
-  end
 end
