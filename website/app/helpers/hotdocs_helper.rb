d8347fd2f7dc6d161457805a17e849dfbfbfc470
3v0k4 <riccardo.odone@gmail.com>
2025-04-03 08:35:34 +0200

    chore: improve install task

---
+++ website/app/helpers/hotdocs_helper.rb
@@ -0,0 +1,74 @@
+module HotdocsHelper
+  def logo
+    Struct.new(:src, :alt).new(asset_path("hotdocs.svg"), "A humanized and happy hot dog")
+  end
+
+  def nav_left_items(classes)
+    [
+      active_link_to("Docs", root_path, class: Array(classes))
+    ]
+  end
+
+  def nav_right_items(classes)
+    [
+      external_link_to("GitHub", "https://github.com/3v0k4/hotdocs", class: Array(classes))
+    ]
+  end
+
+  def menu_items
+    [
+      { label: "Welcome", url: root_path },
+      {
+        label: "Quickstart", url: quickstart_path, children: [
+          { label: "Embedded", url: embedded_path },
+          { label: "Standalone", url: standalone_path }
+        ]
+      },
+      { label: "Markdown", url: markdown_path },
+      { label: "Static export", url: static_export_path },
+      {
+        label: "Components", url: components_path, children: [
+          { label: "Nav / Sidenav", url: nav_path },
+          { label: "Table of contents", url: toc_path },
+          { label: "Search", url: search_path },
+          { label: "Light / Dark", url: light_dark_path },
+          { label: "Footer", url: footer_path },
+          { label: "Helpers", url: helpers_path }
+        ]
+      }
+    ]
+  end
+
+  def repository_base_url
+    "https://github.com/3v0k4/hotdocs/blob/main"
+  end
+
+  def footer_items
+    [
+      {
+        heading: "Contribute",
+        items: [
+          external_link_to("Source Code", "https://github.com/3v0k4/hotdocs", class: "footer__link")
+        ]
+      },
+      {
+        heading: "Community",
+        items: [
+          external_link_to("GitHub Discussions", "https://github.com/3v0k4/hotdocs/discussions", class: "footer__link")
+        ]
+      },
+      {
+        heading: "(il)Legal",
+        items: [
+          "Nothing to see here."
+        ]
+      },
+      {
+        heading: "HotDocs",
+        items: [
+          "Write your docs with Ruby on Rails."
+        ]
+      }
+    ]
+  end
+end
